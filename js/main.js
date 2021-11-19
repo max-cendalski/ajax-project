63f9843e3f2c8f1fc32f4187aa66b304b85f5a5d
Korneliu <olbiton@gmail.com>
2021-11-19 15:44:43 -0800

    completed user-can-search-for-a-recipe

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ var amountOfServings = 0;
 
 function renderEntry(entry) {
   var liElement = document.createElement('li');
-  liElement.setAttribute('class', 'column-full margin-top20 border-radius10');
+  liElement.setAttribute('class', 'column-width90 margin-top20 border-radius10');
   // liElement.setAttribute('data-entry', entry.entry.id);
 
   var mainRow = document.createElement('div');
@@ -109,32 +109,3 @@ function handleFormSubmit(event) {
 }
 
 $form.addEventListener('submit', handleFormSubmit);
-
-/* var foodXhr = new XMLHttpRequest();
-var name = 'banana pudding';
-var caloriesRange = '100-200';
-var diet = 'balanced';
-foodXhr.open('GET', 'https://api.edamam.com/api/recipes/v2?type=public&q=' + name + '&app_id=e39dceb5&app_key=bffe580698b9b315c25ae296cfb28114&diet=' + diet + '&cuisineType=American&calories=' + caloriesRange + '&imageSize=SMALL&nutrients%5BCHOCDF%5D=10-600&nutrients%5BPROCNT%5D=0-7');
-foodXhr.responseType = 'json';
-foodXhr.addEventListener('load', function () {
-  console.log('response statuts: ', foodXhr.status);
-  console.log('response object', foodXhr.response);
-  console.log('response object', foodXhr.response.hits[1]);
-  console.log('recipes name: ', foodXhr.response.hits[1].recipe.label);
-  console.log('recipes image: ', foodXhr.response.hits[1].recipe.image);
-  console.log('recipes instruction: ', foodXhr.response.hits[1].recipe.url);
-  console.log('recipes ingredients: ', foodXhr.response.hits[1].recipe.ingredients[0].text);
-  console.log('recipes ingredients: ', foodXhr.response.hits[1].recipe.ingredients[1].text);
-  console.log('recipes ingredients: ', foodXhr.response.hits[1].recipe.ingredients[2].text);
-  console.log('TOTAL DAILY CALCIUM: ', Math.floor(foodXhr.response.hits[1].recipe.totalDaily.CA.quantity));
-  console.log('TOTAL DAILY CALCIUM: ', foodXhr.response.hits[1].recipe.totalDaily.CA.unit);
-  console.log('amount of servings: ', foodXhr.response.hits[1].recipe.yield);
-  console.log('amount of calories: ', Math.floor(foodXhr.response.hits[1].recipe.calories));
-  console.log('nutriton type: ', foodXhr.response.hits[1].recipe.totalNutrients.PROCNT.label);
-  console.log('nutrition quantity: ', foodXhr.response.hits[1].recipe.totalNutrients.PROCNT.quantity);
-  console.log('nutriton type: ', foodXhr.response.hits[1].recipe.totalNutrients.CHOCDF.label);
-  console.log('nutrition quantity: ', foodXhr.response.hits[1].recipe.totalNutrients.CHOCDF.quantity);
-  console.log('nutriton type: ', foodXhr.response.hits[1].recipe.totalNutrients.SUGAR.label);
-  console.log('nutrition quantity :', foodXhr.response.hits[1].recipe.totalNutrients.SUGAR.quantity);
-});
-foodXhr.send(); */
